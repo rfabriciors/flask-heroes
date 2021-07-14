08d398d32ffe43a7e6714d76545852b9e50b2345
rfabriciors <rfabriciors@gmail.com>
2021-07-14 11:17:25 -0300

    Commit 2 em dev

--- app_heroes.py
+++ app_heroes.py
@@ -9,5 +9,10 @@ def index():
     data = requests.get('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json')
     return data.json()
 
+@app.route("/usuarios", methods=["GET"])
+def index():
+    data = requests.get('https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb')
+    return data.json()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port="5000")
